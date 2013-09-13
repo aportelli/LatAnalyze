c4751f20a3080c384551b31da47405be465f6d04
Antonin Portelli <antonin.portelli@me.com>
2013-09-13 17:06:59 +0100

    scripts to install Eigen headers properly

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "extracting Eigen headers..."
+./update_eigen.sh eigen-eigen-*.tar.bz2
 rm -rf .buildutils
 mkdir -p .buildutils/m4
 autoreconf -fvi
--- update_eigen.sh
+++ update_eigen.sh
@@ -6,10 +6,13 @@ fi
 ARC=$1
 
 INITDIR=`pwd`
+rm -rf latan/Eigen
 ARCDIR=`tar -tf ${ARC} | head -n1 | sed -e 's@/.*@@'`
 tar -xf ${ARC}
 cd ${ARCDIR}
-rm -rf latan/Eigen
-tar -cf - Eigen --exclude='*.txt' | tar -xvf - -C ../latan/
+tar -cf - Eigen --exclude='*.txt' | tar -xf - -C ../latan/
+cd ../latan
+echo 'eigen_files =\' > eigen_files.mk
+find Eigen -type f -print | sed 's/^/  /;$q;s/$/ \\/' >> eigen_files.mk
 cd ${INITDIR}
 rm -rf ${ARCDIR}
