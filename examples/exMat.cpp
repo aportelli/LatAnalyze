9484934a8afc49f74d0a343c4ed95f88633b8c2f
Antonin Portelli <antonin.portelli@me.com>
2014-02-12 18:35:18 +0000

    improvement of matrix example

--- examples/exMat.cpp
+++ examples/exMat.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <latan/Io.hpp>
 #include <latan/Mat.hpp>
+#include <latan/Math.hpp>
 
 using namespace std;
 using namespace Latan;
@@ -16,15 +17,16 @@ int main(void)
     F.open(fileName, File::Mode::read);
     A = F.read<DMat>("A");
     B = F.read<DMat>("B");
-    cout << "A=\n"   << A   << '\n' << endl;
-    cout << "B=\n"   << B   << '\n' << endl;
-    cout << "A*B=\n" << A*B << '\n' << endl;
+    cout << "A=\n"      << A                         << '\n' << endl;
+    cout << "B=\n"      << B                         << '\n' << endl;
+    cout << "A*B=\n"    << A*B                       << '\n' << endl;
+    cout << "cos(A)=\n" << A.unaryExpr(StdMath::cos) << '\n' << endl;
     F.close();
     
     // write
     cout << "-- saving A*B..." << endl;
     F.open(fileName, File::Mode::append);
-    F.save((A*B).eval(), "AB");
+    F.save(A*B, "AB");
     
     return EXIT_SUCCESS;
 }
