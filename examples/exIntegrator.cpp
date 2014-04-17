39012e4b73f06e8d3417ead47279e1eeb21c8dc1
Antonin Portelli <antonin.portelli@me.com>
2014-04-17 12:17:34 +0100

    GSL integrator added - LatAnalyze now depends on GSL

---
+++ examples/exIntegrator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <LatAnalyze/CompiledFunction.hpp>
+#include <LatAnalyze/GslQagsIntegrator.hpp>
+
+using namespace std;
+using namespace Latan;
+
+int main(int argc, char* argv[])
+{
+    string source;
+    double xMin, xMax;
+    
+    if (argc != 4)
+    {
+        cerr << "usage: " << argv[0] << " <function> <xMin> <xMax>" << endl;
+        
+        return EXIT_FAILURE;
+    }
+    source = argv[1];
+    xMin   = strTo<double>(argv[2]);
+    xMax   = strTo<double>(argv[3]);
+    
+    CompiledDoubleFunction f(1, source);
+    GslQagsIntegrator      integrator;
+    double                 result;
+    
+    result = integrator(f, xMin, xMax);
+    cout << "function integral on [" << xMin << ", " << xMax << "] = ";
+    cout << result << " +/- " << integrator.getLastError() <<endl;
+    
+    return EXIT_SUCCESS;
+}
---
+++ lib/Integrator.hpp
@@ -0,0 +1,46 @@
+/*
+ * Integrator.hpp, part of LatAnalyze 3
+ *
+ * Copyright (C) 2013 - 2014 Antonin Portelli
+ *
+ * LatAnalyze 3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LatAnalyze 3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LatAnalyze 3.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef Latan_Integrator_hpp_
+#define Latan_Integrator_hpp_
+
+#include <LatAnalyze/Global.hpp>
+#include <LatAnalyze/Function.hpp>
+
+BEGIN_NAMESPACE
+
+/******************************************************************************
+ *                        abstract integrator class                           *
+ ******************************************************************************/
+
+class Integrator
+{
+public:
+    // constructor
+    Integrator(void) = default;
+    // destructor
+    virtual ~Integrator(void) = default;
+    // integral calculation
+    virtual double operator()(const DoubleFunction &f, const double xMin,
+                              const double xMax) = 0;
+};
+
+END_NAMESPACE
+
+#endif // Latan_Integrator_hpp_
