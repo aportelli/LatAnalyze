4b5ad9014c6f2e5960fc3e185e426028098f29f7
Antonin Portelli <antonin.portelli@me.com>
2020-01-10 14:15:40 +0000

    more script tuning

--- ci-scripts/install-latan.sh
+++ ci-scripts/install-latan.sh
@@ -5,14 +5,17 @@ if (( $# != 1 )); then
   exit 1
 fi
 PREFIX=$1
-OS=$2
 
 set -ex
+INITDIR=$(pwd -P)
+cd ${PREFIX}
+PREFIX=$(pwd -P)
+cd ${INITDIR}
 ./install-deps.sh ${PREFIX}
 cd ..
 ./bootstrap.sh
 mkdir -p build
 cd build
-../configure --prefix=$PREFIX --with-minuit=$PREFIX --with-nlopt=$PREFIX --with-hdf5=$PREFIX --with-gsl=$PREFIX CXXFLAGS="${CXXFLAGS} -O3 -march=native -mtune=native"
+../configure --prefix=${PREFIX} --with-minuit=${PREFIX} --with-nlopt=${PREFIX} --with-hdf5=${PREFIX} --with-gsl=${PREFIX} CXXFLAGS="${CXXFLAGS} -O3 -march=native -mtune=native"
 make -j4
 make install
