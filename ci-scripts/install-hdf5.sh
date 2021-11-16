d6e5ba724d7767a5a97b4085d29d5258f90b76d9
Antonin Portelli <antonin.portelli@me.com>
2021-11-16 21:35:35 +0000

    CI scripts expose number of build tasks

--- ci-scripts/install-hdf5.sh
+++ ci-scripts/install-hdf5.sh
@@ -2,11 +2,12 @@
 
 NAME='hdf5-1.10.5'
 
-if (( $# != 1 )); then
-  echo "usage: `basename $0` <prefix>" 1>&2
+if (( $# != 2 )); then
+  echo "usage: `basename $0` <prefix> <ntasks>" 1>&2
   exit 1
 fi
 PREFIX=$1
+NTASKS=$2
 
 set -ex
 INITDIR=$(pwd -P)
@@ -19,7 +20,7 @@ tar -xzvf ${NAME}.tar.gz
 mkdir ${NAME}/build
 cd ${NAME}/build
 ../configure --prefix=${PREFIX} --enable-cxx
-make -j4 
+make -j${NTASKS}
 make install
 cd ${INITDIR}/local
 touch .built.hdf5
--- ci-scripts/install-minuit.sh
+++ ci-scripts/install-minuit.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 
-if (( $# != 1 )); then
-  echo "usage: `basename $0` <prefix>" 1>&2
+if (( $# != 2 )); then
+  echo "usage: `basename $0` <prefix> <ntasks>" 1>&2
   exit 1
 fi
 PREFIX=$1
+NTASKS=$2
 
 set -ex
 INITDIR=$(pwd -P)
@@ -18,7 +19,7 @@ git clone https://github.com/root-project/root.git
 cd root/math/minuit2/
 mkdir build; cd build
 cmake .. -Dminuit2_standalone=ON -DCMAKE_INSTALL_PREFIX=${PREFIX}
-make -j4 
+make -j${NTASKS}
 make install
 cd ${INITDIR}/local
 touch .built.minuit
